0588596e64e539e7a0c4aefb23ea4deac0d727e7
unknown <p6579113@gmail.com>
2024-09-19 20:36:41 +0530

    donee

--- looper/q1.c
+++ looper/q1.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 main(){
+	//Inilisation of variable
 	char letter='a';
-	
+	//Applying do-while loop
 	do{
+
 		printf(" %c ",letter);
+		//Incrementing by 4 acc to question
 		letter=letter+4;		
 	}
+	//Final condition
 	while(letter<='z');
 }
--- looper/q2.c
+++ looper/q2.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 main(){
-
+    //Inilization of digit variable and declaring number variable
   int number,digit=0;  
 
   printf("Enter number of your choice:\n");
   scanf("%d",&number);
-
+    //Applyinh while loop
   while(number!=0)
     {
       number=number/10;
       digit++;
     }
+    //Required result
   printf("Here,the result is %d",digit);
 }
