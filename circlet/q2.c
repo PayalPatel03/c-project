1977d876280aba08e783959da577f4fa52a771a4
unknown <p6579113@gmail.com>
2024-09-25 21:39:05 +0530

    donee

---
+++ circlet/q2.c
@@ -0,0 +1,20 @@
+//11
+//12  13
+//14  15  16
+//17  18  19  20
+
+
+#include<stdio.h>
+main(){
+	int i,j,begin=11;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf(" %d ",begin);
+			begin++;
+		}
+		printf("\n");
+	}
+	
+}
